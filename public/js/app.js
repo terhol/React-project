b90e9594194eef415ea7fe86645e7f9d8ee411a2
Tereza Holm <terhol@Tereza-MacBook-Pro.local>
2020-06-06 21:25:55 +0200

    ProductList Component added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                Hello! I am a basic React Component. What I learned by this? Well, that react Components always start
+                with an upper case :-)
+            </div>
+        );
+    }
+}
 
+ReactDOM.render(<ProductList/>, document.getElementById('content'));
